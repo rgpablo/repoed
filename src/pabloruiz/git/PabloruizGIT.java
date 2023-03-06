ed5cd010bbf2bc70dd904a404094ac5076a784bf
Pablo <Pablo@DESKTOP-TMFO3T5>
2023-03-06 22:02:32 +0100

    Versión inicial. Valor = 1

---
+++ src/pabloruiz/git/PabloruizGIT.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pabloruiz.git;
+
+/**
+ *
+ * @author Pablo
+ */
+public class PabloruizGIT {
+    
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        // Alumno: Pablo Ruiz García
+         int valor;
+         
+         valor = 1;
+         
+         System.out.println("Valor= "+valor);
+    }
+    
+}
